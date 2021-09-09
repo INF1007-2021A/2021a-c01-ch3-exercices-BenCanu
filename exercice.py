8913559ff60ed33cfe2a78f26d5cc72d5dc5edea
BenCanu <bcanuel26@hotmail.com>
2021-09-09 14:46:16 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -4,32 +4,40 @@
 
 import math
 
+
 def square_root(a: float) -> float:
-    return 0.0
+    return math.sqrt(a)
+
 
 
 def square(a: float) -> float:
-    return 0.0
+    return a ** 2
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return (a + b + c) / 3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    b = angle_mins / 60
+    c = angle_secs / 3600
+    a = math.radians(angle_degs + b + c)
+    return a
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    a = math.degrees(angle_rads)
+    b = a * 60
+    c = b* 60
+    return a, b ,c
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature - 32) * 5 / 9
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return (temperature * 9/5) + 32
 
 
 def main() -> None:
@@ -39,7 +47,7 @@ def main() -> None:
 
     print(f"Moyenne des nombres 2, 4, 6: {average(2, 4, 6)}")
 
-    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
+    print(f"Conversion de 180 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
     
     degrees, minutes, seconds = to_degrees(1.0)
     print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
